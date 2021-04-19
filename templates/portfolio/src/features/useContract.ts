1c3202dfaea12437298a26e5dd3e0d8b7aa2c5bd
shahruz <hello@shahr.uz>
2021-04-19 00:38:28 -0700

    update contract name

--- templates/portfolio/src/features/useContract.ts
+++ templates/portfolio/src/features/useContract.ts
@@ -1,11 +1,11 @@
-import { ERC721BasicPortfolio__factory } from '@gimmixfactory/contracts/dist/typechain';
+import { Portfolio__factory } from '@gimmixfactory/contracts/dist/typechain';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { useWallet } from '@gimmixfactory/use-wallet';
 import factoryConfig from '../../factory.config';
 
 const useContract = () => {
   const { provider, network } = useWallet();
-  const contract = ERC721BasicPortfolio__factory.connect(
+  const contract = Portfolio__factory.connect(
     factoryConfig.contractAddress,
     !!provider && network?.chainId == factoryConfig.chainId
       ? provider.getSigner()
