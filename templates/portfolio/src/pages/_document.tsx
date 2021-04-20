c37437c27139cea48e22656239c4140a4c0ab583
shahruz <hello@shahr.uz>
2021-04-19 17:28:23 -0700

    update regex

--- templates/portfolio/src/pages/_document.tsx
+++ templates/portfolio/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 const scriptTxt = `
 (function () {
   const { pathname } = window.location
-  const ipfsMatch = /.*\\/Qm\\w{44}\\//.exec(pathname)
+  const ipfsMatch = /.*\\/Qm\\w{44}/.exec(pathname)
   const base = document.createElement('base')
 
   base.href = ipfsMatch ? ipfsMatch[0] : '/'
