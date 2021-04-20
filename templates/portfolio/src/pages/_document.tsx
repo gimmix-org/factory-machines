457e5da9f3b7bbd59c2f007a6b30a0ff263c71a0
shahruz <hello@shahr.uz>
2021-04-19 18:37:54 -0700

    fix ipfs

--- templates/portfolio/src/pages/_document.tsx
+++ templates/portfolio/src/pages/_document.tsx
@@ -3,9 +3,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 const scriptTxt = `
 (function () {
   const { pathname } = window.location
-  const ipfsMatch = /.*\\/Qm\\w{44}/.exec(pathname)
+  const ipfsMatch = /.*\\/Qm\\w{44}\\//.exec(pathname)
   const base = document.createElement('base')
-
   base.href = ipfsMatch ? ipfsMatch[0] : '/'
   document.head.append(base)
 })();
